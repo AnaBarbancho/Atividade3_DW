96950d3a674c5e42a448ddbac842af6c5cb2567c
LAB-80 <ana.afonso2@fatec.sp.gov.br>
2024-04-22 20:32:12 -0300

    exercicio3

---
+++ src/routes/index.tsx
@@ -0,0 +1,43 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Menu from "../components/Menu";
+import styled from "styled-components";
+import HSLA from "../pages/HSLA";
+import CMYK from "../pages/CMYK";
+import RGB from "../pages/RGB";
+
+export default function Rotas() {
+  return (
+    <PageSld>
+      <BrowserRouter>
+        <Menu />
+        <BodySld>
+          <Routes>
+            <Route path="/rgb" element={<RGB />}/>
+            <Route path="/hsla" element={<HSLA />}/>
+            <Route path="/cmyk" element={<CMYK />}/>
+          </Routes>
+        </BodySld>
+      </BrowserRouter>
+    </PageSld>
+  );
+}
+
+const PageSld = styled.div`
+  display: flex;
+`;
+
+const BodySld = styled.div`
+  position: absolute; /* Alterado para position: absolute */
+  top: calc(
+    50% + 60px
+  ); /* Posiciona o elemento no meio verticalmente e Ajusta o valor para compensar a altura do menu */
+  left: 50%; /* Posiciona o elemento no meio horizontalmente */
+  transform: translate(
+    -50%,
+    -50%
+  ); /* Move o elemento de volta metade de sua própria largura e altura */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+`;
